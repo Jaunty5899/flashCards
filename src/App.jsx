5774086865b1715b595ab8ea628e9cf2e3c745e4
Jaunty <jaunty5899@gmail.com>
2025-01-14 15:31:24 +0530

    data added&card fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import { useState } from "react";
 import "./App.css";
 
+let data = [
+  {
+    question: "What is the capital of France?",
+    answer:
+      "Paris, a vibrant city known for iconic landmarks like the Eiffel Tower and the Louvre.",
+  },
+  {
+    question: "What is the largest ocean in the world?",
+    answer:
+      "The Pacific Ocean, covering 63 million sq miles and known for its diverse marine life.",
+  },
+  {
+    question: "What is the smallest planet in our solar system?",
+    answer:
+      "Mercury, the closest planet to the Sun with a diameter of just 3,031 miles.",
+  },
+  {
+    question: "What is the currency used in Japan?",
+    answer:
+      "The Japanese Yen, one of the world's major currencies and a symbol of Japan's strong economy.",
+  },
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="container">
@@ -13,10 +36,10 @@ function App() {
       </div>
       <div className="subContainer flashCard">
         <div className="element card">
-          <div class="subElementBody" id="result">
+          <div class="subElementBody" id="question">
             hello
           </div>
-          <div class="subElementBody" id="summary">
+          <div class="subElementBody" id="answer">
             world
           </div>
         </div>
